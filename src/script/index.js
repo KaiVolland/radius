5dd4a2803fef878994cb56e4ccc3c78b97570a4c
Kai Volland <volland@terrestris.de>
2022-06-19 16:55:54 +0200

    adapt radius

--- src/script/index.js
+++ src/script/index.js
@@ -1,8 +1,11 @@
 function showRadius(event) {
-  console.log('blalba', event);
   const container = document.querySelector('.container');
-  container.style.setProperty('--radius', '300px');
-  const { x, y } = event;
+  container.style.setProperty('--radius0', '0px');
+  container.style.setProperty('--radius1', '380px');
+  container.style.setProperty('--radius2', '470px');
+  container.style.setProperty('--radius3', '580px');
+  container.style.setProperty('--radius4', '900px');
+  const { offsetX: x, offsetY: y } = event;
   container.style.setProperty('--x', x + 'px');
   container.style.setProperty('--y', y + 'px');
 };
